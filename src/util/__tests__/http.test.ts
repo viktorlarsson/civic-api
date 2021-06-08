1d78853da5c6d23541b9688f1af138dc566173ab
Viktor Sarström <viktorsarstrom@gmail.com>
2021-06-08 09:26:34 +0200

    Fixes set credentials test

--- src/util/__tests__/http.test.ts
+++ src/util/__tests__/http.test.ts
@@ -1,9 +1,17 @@
-import { convertToQueryString } from '../http';
+import axios from 'axios';
+import { convertToQueryString, setCredentials } from '../http';
 
 describe('http', () => {
   it('should convert an object to query string params', () => {
     const object = { foo: 'bar', bar: 1 };
 
     expect(convertToQueryString(object)).toEqual('?foo=bar&bar=1');
+  });
+
+  it('set credentials', () => {
+    setCredentials('yolo123', 'foobar')
+
+    expect(axios.defaults.headers.get['client_id']).toEqual('yolo123');
+    expect(axios.defaults.headers.get['client_secret']).toEqual('foobar');
   });
 });
